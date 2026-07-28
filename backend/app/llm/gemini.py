8370ad7101eb2f40bbe333dd475512452877e62a
pani2004 <debashispani61@gmail.com>
2026-07-29 00:35:31 +0530

    Add LLM initialization and update error message in gemini.py; create new __init__.py files for llm and schemas

--- backend/app/llm/gemini.py
+++ backend/app/llm/gemini.py
@@ -1,12 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+
 from app.config import GOOGLE_API_KEY, MODEL_NAME
 
 
-def get_llm()-> ChatGoogleGenerativeAI:
+def get_llm(temperature: float = 0.2) -> ChatGoogleGenerativeAI:
     if not GOOGLE_API_KEY:
-        raise ValueError("Google Api Key is not set")
+        raise ValueError("GOOGLE_API_KEY is not set in .env")
 
     return ChatGoogleGenerativeAI(
         model=MODEL_NAME,
         google_api_key=GOOGLE_API_KEY,
+        temperature=temperature,
     )
